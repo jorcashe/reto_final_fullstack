864b79584d272ef2ce42e45c32995014858e411f
jorcashe <jcastrillonhdez@gmail.com>
2026-04-24 16:48:06 -0500

    20. Desarrollo de la vista detallada del Carrito de compras

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import { useCartStore } from "../store/useCartStore";
 
+
 const Cart = () => {
   const { cart, removeFromCart, getTotal } = useCartStore();
 
