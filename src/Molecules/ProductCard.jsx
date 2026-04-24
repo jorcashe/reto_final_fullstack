87294d65df52d6c433fffe1960d5de99c6333c1d
jorcashe <jcastrillonhdez@gmail.com>
2026-04-24 16:40:32 -0500

    13. Creación de la molécula ProductCard

--- src/Molecules/ProductCard.jsx
+++ src/Molecules/ProductCard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useCartStore } from "../store/useCartStore";
 
+
 const ProductCard = ({ product }) => {
   const addToCart = useCartStore((state) => state.addToCart);
 
