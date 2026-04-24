7a1e4144ff2b56580114f6435f52c134333fa0d5
jorcashe <jcastrillonhdez@gmail.com>
2026-04-24 16:50:20 -0500

    22. Optimización de imágenes para carga rápida

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// Aquí es donde ocurre el error si "root" no coincide con el ID del HTML
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
