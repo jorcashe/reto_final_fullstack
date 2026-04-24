9e13927522157d9674e533abe5e35c662162ed80
jorcashe <jcastrillonhdez@gmail.com>
2026-04-24 16:47:02 -0500

    18. Ajuste de grid responsivo a 3 columnas para escritorio

--- src/Organisms/ProductGallery.jsx
+++ src/Organisms/ProductGallery.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { products } from "../mockdata/products";
 import ProductCard from "../Molecules/ProductCard";
 
-
+//Const
 const ProductGallery = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 p-4">
